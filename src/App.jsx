c42e5800a903a7bdabe0d465a3026b0206990998
Alex Miranda <alexxmirandaa@gmail.com>
2019-04-27 23:12:44 -0300

    Resolvendo caminho do styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import { ConnectedRouter } from 'react-router-redux';
-import 'styles/global.scss';
+import './styles/global.scss';
 // import Header from './components/Header';
 import getRoutes from './containers/Routes';
 import store from './redux-flow';
